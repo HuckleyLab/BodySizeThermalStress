56f7617966a9e1781f35d66ec38752780c0429d5
Lauren Buckley <lbbuckley@gmail.com>
2020-08-27 11:26:48 -0700

    Update analysis

--- Fig1.R
+++ Fig1.R
@@ -1,4 +1,4 @@
-#Alanysis for a NCC news and views
+#Analysis for a NCC news and views
 
 library(zoo)
 
@@ -7,7 +7,7 @@ setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/NCCnews/data/")
 #load climate data
 clim= read.csv("BoulderCr.csv")
 temp=clim$TempC
-#potential sources to update cliamte data:
+#potential sources to update climate data:
 #https://climate.weather.gc.ca/historical_data/search_historic_data_e.html
 #https://data.pacificclimate.org/portal/pcds/map/
 #ftp://ftp.ncdc.noaa.gov/pub/data/uscrn/products/subhourly01/2019/
@@ -16,27 +16,18 @@ temp=clim$TempC
 #https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/1365-2656.12789
 #Scaphinotus angusticollis
 
-bs= read.csv("TsengDryad.csv")
-bs= subset(bs,bs$species=="angusticollis") 
-bl= 1.56*bs$Elytron.Length 
-dm= 0.03069*bl^2.63885
-bs$mass= dm/0.4
-
-plot(bs$Year, bs$mass)
-
-mod1=lm(bs$mass~bs$Year)
-#1037-0.424*yr
-
-#1920: 1037-0.424*1920= 222.9mg= 0.22
-#2020: 1037-0.424*2020= 180.52mg= 0.18
-
-#ave dry 31.3
+#body masses to model
+#average dry mass= 31.3
 # -0.12% change per year
 #1915 to 2015
+#/0.4 to convert to wet mass as in paper
+#/1000 to convert mg to g
 ml= (31.3 +0.0012*50*31.3)/0.4/1000
 ms= (31.3 -0.0012*50*31.3)/0.4/1000
+#also assume 75% reduction
 mf=ml*0.25
 
+#FUNCTIONS
 #estimate Tcrit
 tcrit= function(m,t){
   ctmax= 41.93 -1.63*log10(m)
@@ -50,17 +41,20 @@ tcrit= function(m,t){
 #find high resolution weather data
 #use rolling average to calculate TSM for each time period
 
+#Average temperature to different time periods then estimate maximum 
+#n is with of period for rolling average
 rollmax= function(n) {
   max(rollmean(temp, n))
 }
 
 #----
-#PLOT
+#FIGURE
 
-#A. Tcrit
+#Fig 1A. Tcrit and Ta over time
 hrs=1:168
 rt= sapply( hrs, FUN=rollmax)
 
+#Estimate Tcrits for 3 sizes
 tcrits.l= tcrit(ml,hrs*60)
 tcrits.s= tcrit(ms,hrs*60)
 tcrits.f= tcrit(mf,hrs*60)
@@ -74,7 +68,7 @@ labs= c("1 hour","6 hours","1 day", "1 week")
 xs= log10( c(1,6,24, 168))
 
 plot(log10(hrs), tcrits.l, col="blue", type="l", 
-     ylab="temperature (C)", xlab="log10 time",cex.lab=1.2, xaxt="n")
+     ylab="temperature (°C)", xlab="log10 time",cex.lab=1.2, xaxt="n")
 points(log10(hrs), tcrits.s, col="orange", type="l")
 points(log10(hrs), tcrits.f, col="green", type="l")
 #plot temps
@@ -88,7 +82,7 @@ legend("topright", legend=c("1915: 82.9mg", "2015: 73.5mg","20.7mg", "Ta"),
        col=c("blue", "orange", "green","black"), lty=c("solid","solid","solid","dashed"), cex=1.2)
 
 #-----------------
-#B. mass specific, CHANGE TO TIME AS X AXIS
+#Fig 1B. Plot change in maximum metabolism at Tcrit as a function of exposure time
 a <- 2
 b <- 0.75
 Q10 <- 2.5
@@ -108,7 +102,7 @@ plot(log10(hrs), log10MR(ml,hrs*60), type="l", ylab="log10 metabolic rate (W)", 
      col="blue",cex.lab=1.2, xaxt="n", ylim= range(0.25,1) ) 
 points(log10(hrs), log10MR(ms,hrs*60), col="orange", type="l")
 points(log10(hrs), log10MR(mf,hrs*60), col="green", type="l")
-text(0,1.6,"b")
+text(0,1,"b")
 #update axis
 axis(1, at=xs, labels=labs)
 
@@ -122,13 +116,14 @@ text(1,0.78,"slope= -0.134",col="orange", srt=-35, cex=1.2)
 text(1,0.47,"slope= -0.144",col="green", srt=-35, cex=1.2)
 
 #-----------------
-#C. TSM
-#calc tsm
+#Fig 1C. Plot TSM as a function of exposure time
+
+#Estimate TSM
 tsm.l= tcrits.l -rt 
 tsm.s= tcrits.s -rt 
 tsm.f= tcrits.f -rt
 
-plot(log10(hrs), tsm.l, type="l", ylab="thermal safety margins (C)", xlab="log10 time", 
+plot(log10(hrs), tsm.l, type="l", ylab="thermal safety margin (TSM, °C)", xlab="log10 time", 
      col="blue",cex.lab=1.2, xaxt="n")
 points(log10(hrs), tsm.s, col="orange", type="l")
 points(log10(hrs), tsm.f, col="green", type="l")
